b6d78e4b87caa3a34f48c33ffea8c75c18f802b3
MatiasBorquez <matiasalzogaray2@gmail.com>
2022-06-14 20:18:49 -0300

    first commit

---
+++ src/main/java/datos/IClienteDaoJDBC.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+import dominio.Cliente;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Boros
+ */
+public interface IClienteDaoJDBC {
+    public List<Cliente> listar() throws SQLException;
+    public Cliente encontrar(Cliente cliente) throws SQLException;
+    
+    public int insertar(Cliente cliente) throws SQLException;
+    
+    public int editar(Cliente cliente) throws SQLException;
+    
+    public int eliminar(Cliente cliente) throws SQLException;
+}
